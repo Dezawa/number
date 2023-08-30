548b01d7f46d8d9664a66c4f2e250bffb5450c25
Dezawa <dezawa@aliadne.net>
2023-08-30 20:17:03 +0900

    リファクタxross optional_group

--- lib/number/game_types/xross.rb
+++ lib/number/game_types/xross.rb
@@ -12,13 +12,21 @@ module Number
       def optional_group(gnr, boxes, xmax, cells)
         return 0 if boxes.size != 1
 
+        xross_groups(gnr)
+        xross_cells(cells, gnr, xmax)
+        gnr + 2
+      end
+
+      def xross_groups(gnr)
         @groups[gnr] = Number::Group.new(self, gnr, @count, :xross)
         @groups[gnr + 1] = Number::Group.new(self, gnr + 1, @count, :xross)
+      end
+
+      def xross_cells(cells, gnr, xmax)
         (0..game_scale - 1).each do |i|
           cells[(xmax + 1) * i].group_ids << gnr
           cells[game_scale - 1 + (xmax - 1) * i].group_ids << gnr + 1
         end
-        gnr + 2
       end
     end
   end
