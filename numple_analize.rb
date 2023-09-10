4566ac892c6fd30188858eea977dba2ffb14e431
Dezawa <dezawa@aliadne.net>
2023-09-10 18:36:32 +0900

    numple7DL用script

---
+++ numple_analize.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+# == dir内のgameを全部解いて
+# default :: 解けなかった問題をlist up
+# -S      :: 解けたか否かに関わらず、使った技の統計を出す
+# frozen_string_literal: true
+
+require_relative 'lib/number/game'
+
+# == dir内のgameを全部解いて
+# default :: 解けなかった問題をlist up
+# -S      :: 解けたか否かに関わらず、使った技の統計を出す
+class NumpleAnalize
+  attr_accessor :directories
+   
+  def numples
+    @numples = [directories].flatten.map{|directory| Dir.glob("#{directory}/*")}.flatten
+  end
+
+  def analyze
+    numples.map do |numple|
+      puts numple
+      game = Number::Game.create(File.open(numple))
+      numple unless game.resolve
+    end.compact
+  end
+
+  def output
+    pp analyze
+  end
+end
