f66968125ba746409aa5c6f2bf85b66ba1e01511
dezawa <dezawa@aliadne.net>
2026-01-30 17:41:27 +0900

    numpre7分析関係code

--- numpre7/bin/numpre7.rb
+++ numpre7/bin/numpre7.rb
@@ -1,8 +1,22 @@
 # nampre7.rb
-# ナンプレ７のサイトから game(npLNN001〜)をDLし得た html を返す
+# ナンプレ７のサイトから game(npLNN001〜)をDLし
+# gameのデータを取り出す。 空白は ドット、数字が有ったらその値
+# ...34....
+# .9.......
+# .6...72.4
+# .2..3.756
+# .........
+# 675.8..3.
+# 7.39...2.
+# .......8.
+# ....26...
+# というgameを
+# ...34.....9........6...72.4.2..3.756.........675.8..3.7.39...2........8.....26...
+# で表現
+
 require "ferrum"
 
-class Nampre7
+class Numpre7
   BASE_URL = "https://numpre7.com"
 
   def initialize(timeout: 30, headless: true)
@@ -57,6 +71,22 @@ end
   #   page&.close
   # end
 
+
+  # HTML → 盤面文字列
+  def bord(html_str)
+    cells = html_str
+              .split("</div>")
+              .select { |line| /eg-obj eg-np x/ =~ line }
+
+    cells = cells.map do |cell|
+      v = cell.sub(/.*>/, "")
+      v =~ /\d/ ? v : "."
+    end
+
+    cells = cells.each_slice(9).to_a.transpose.flatten.join
+    cells.size < 9 ? false : cells
+  end
+  
   def close
     @browser&.quit
   end
